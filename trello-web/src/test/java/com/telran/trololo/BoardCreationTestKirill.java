8a89cd055e39e0aa5e7098236f460da404ced39a
ZborovskiyKyryl <speak.zborovskiy@gmail.com>
2019-09-10 19:58:02 +0300

    BoardCreationTest

---
+++ trello-web/src/test/java/com/telran/trololo/BoardCreationTestKirill.java
@@ -0,0 +1,19 @@
+package com.telran.trololo;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class BoardCreationTestKirill extends TestBase {
+
+    @Test
+    public void mainTest() throws InterruptedException {
+
+        boardName = "newBoard5.1";
+        createNewBoard(boardName);
+        Assert.assertTrue(isNewBoardCreatedBySize());
+        //Assert.assertTrue(isNewBoardCreatedByName());
+    }
+
+
+}
